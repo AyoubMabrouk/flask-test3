188ea4a412f2dad66ca1cc43675ec3e4542a1ca3
Ayoub Mabrouk <73364731+AyoubMabrouk@users.noreply.github.com>
2023-12-30 14:02:51 +0100

    Update predictRejetCCE.py

--- predictRejetCCE.py
+++ predictRejetCCE.py
@@ -1,15 +1,21 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 paiement_data=pd.read_csv('pa.txt', sep = ';')
 
-X = paiement_data.loc[:, ['Etat', 'IdTypeLot','BanquePayeur','MotifRejet']]
-y = paiement_data['RejetCCE']
+paiement_dataEncoded=paiement_data.copy()
+labelencoder = LabelEncoder()
+paiement_dataEncoded['Etat'] = labelencoder.fit_transform(paiement_data['Etat'])
+paiement_dataEncoded['IdTypeLot'] = labelencoder.fit_transform(paiement_data['IdTypeLot'])
+paiement_dataEncoded['BanquePayeur'] = labelencoder.fit_transform(paiement_data['BanquePayeur'])
+paiement_dataEncoded['MotifRejet'] = labelencoder.fit_transform(paiement_data['MotifRejet'])
+paiement_dataEncoded['RejetCCE'] = labelencoder.fit_transform(paiement_data['RejetCCE'])
 
+X = paiement_dataEncoded.loc[:, ['Etat', 'IdTypeLot','BanquePayeur','MotifRejet']]
+y = paiement_dataEncoded['RejetCCE']
 from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.3)
-
 from sklearn.tree import DecisionTreeClassifier
-
 def training_model():
     model = DecisionTreeClassifier()
     trained_model = model.fit(X_train, y_train)
